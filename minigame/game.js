60087abfc792b9eae538416326ff5c5edc85d25c
PerrinYong <PerrinYong@163.com>
2026-01-06 21:53:07 +0800

    new

--- minigame/game.js
+++ minigame/game.js
@@ -9,7 +9,7 @@ import checkVersion from './check-version';
 import { launchEventType, scaleMode } from './plugin-config';
 import { preloadWxCommonFont } from './unity-sdk/font/index';
 const managerConfig = {
-    DATA_FILE_MD5: '47ea1a75a1de0f83',
+    DATA_FILE_MD5: '3adeb13d3512d0c5',
     CODE_FILE_MD5: 'e26fb6e4cba1aa15',
     GAME_NAME: 'webgl',
     APPID: 'wxf881c14d904294e5',
